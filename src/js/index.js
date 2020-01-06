5e8b9229e01df5296db7bc0b4803f4c06b33cc61
thanhtung3496 <33385100+thanhtung3496@users.noreply.github.com>
2020-01-06 16:38:41 -0500

    finish project install and set-up

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,36 @@
-// Global app controller
-import num from './test';
-console.log(`I imported ${num} from another module`);
+import Search from './models/Search';
+
+/* GLOBAL STATE of the app
+** - Search object
+** - Current recipe object
+** - Shopping list object
+** - Liked recipes
+*/ 
+const state = {};
+
+const controlSearch = async () => {
+    //1. get the query from the view
+    const query = 'pizza'; //TODO
+    
+    if (query) { 
+        //2. new search object and add to the state
+        state.search = new Search(query);
+        
+        //3. prepare UI for results
+        
+        //4. search for recipes
+        await state.search.getResults();
+        
+        //5. show the results on UI
+        console.log(state.search.result);
+    }
+}
+
+document.querySelector('.search').addEventListener('submit', e => {
+    e.preventDefault();
+    controlSearch();
+});
+
+const search = new Search('pizza');
+console.log(search);
+search.getResults();
